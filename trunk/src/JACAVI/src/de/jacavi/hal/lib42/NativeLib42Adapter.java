f5bb9b7780025e09cc3e5dfac212b2644fba2e38
froth <froth@4031bf02-2d4d-0410-8032-e70447eaef8d>
2008-07-31 15:13:29 +0000

    [Related to ticket #4] updated NativeLib42 and the NativeLib42Adapter

--- trunk/src/JACAVI/src/de/jacavi/hal/lib42/NativeLib42Adapter.java
+++ trunk/src/JACAVI/src/de/jacavi/hal/lib42/NativeLib42Adapter.java
@@ -1,9 +1,8 @@
 package de.jacavi.hal.lib42;
 
-import de.jacavi.hal.CarController;
 import de.jacavi.hal.lib42.NativeLib42;
 
-public class NativeLib42Adapter implements CarController {
+public class NativeLib42Adapter implements Clib42CarreraController{
 
 	private NativeLib42 lib42=null;
 	
@@ -57,7 +56,7 @@ public class NativeLib42Adapter implements CarController {
     @Override
     public void activateFuel() {
         
-        // TODO Auto-generated method stub
+        lib42.activateFuel();
          
     }
 
@@ -75,8 +74,7 @@ public class NativeLib42Adapter implements CarController {
 
     @Override
     public void programmCar(int carID) {
-        // TODO Auto-generated method stub
-        
+        lib42.programmCar(carID);
     }
 
     @Override
@@ -95,6 +93,54 @@ public class NativeLib42Adapter implements CarController {
     public void resetCars() {
         // TODO Auto-generated method stub
         
+    }
+
+    @Override
+    public void activatePacecar() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void deactivatePacecar() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public int getPcSwitch() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public int isPacecarActive() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public void pacecar2box() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void setPacecarSwitch(int value) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void setPcPitstop(int pitstop) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public int togglePacecarSwitch() {
+        // TODO Auto-generated method stub
+        return 0;
     }
     
 }
