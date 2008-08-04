bf8c3659d9693117febac12de5b7e883adbd019b
hheimbuerger <hheimbuerger@4031bf02-2d4d-0410-8032-e70447eaef8d>
2008-08-04 04:30:50 +0000

    [In preparation of #18 and #5] Improved in-memory data structures for storing the slot car track.
    The Track class has been extended by some methods (most of which aren't implemented yet) and a new class TrackSection has been created that is supposed to represent one section of the whole track.
    
    Also added three section bitmaps for testing, taken from the track bitmap Fabi found somewhere. These are of course not the final images, only placeholders.

--- trunk/src/JACAVI/src/de/jacavi/appl/valueobjects/Track.java
+++ trunk/src/JACAVI/src/de/jacavi/appl/valueobjects/Track.java
@@ -1,5 +1,39 @@
 package de.jacavi.appl.valueobjects;
 
+import java.util.LinkedList;
+import java.util.List;
+
+import org.eclipse.swt.graphics.Point;
+
+
+
 public class Track {
+
+    private List<TrackSection> sections = new LinkedList<TrackSection>();
+
+    public Track() {
+        sections.add(new TrackSection("finishing_straight.png", new Point(106, 0), 0));
+    }
+
+    public void insertSection(TrackSection section, int position) {
+        sections.add(position, section);
+    }
+
+    public void removeSection(int position) {
+        throw new RuntimeException("Not yet implemented.");
+    }
+
+    public List<TrackSection> getSections() {
+        return sections;
+    }
+
+    public void saveToXml(String filename) {
+        throw new RuntimeException("Not yet implemented.");
+    }
+
+    public void loadFromXml(String filename) {
+        // TODO: invoke from constructor and make private?
+        throw new RuntimeException("Not yet implemented.");
+    }
 
 }
