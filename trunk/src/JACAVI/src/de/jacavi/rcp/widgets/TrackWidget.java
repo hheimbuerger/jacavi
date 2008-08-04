0f02cfa5df5d4d13ddfc85f1ef42d15d7d77ec2c
hheimbuerger <hheimbuerger@4031bf02-2d4d-0410-8032-e70447eaef8d>
2008-08-04 04:34:04 +0000

    [Partial fix for #5] Added early version of a real track view.
    An early version of the track view has been created. While this is in fact already using seperate images for the sections and does take its track data from the in-memory data structure, how it is using the data structure has to be overhauled majorly in the future. Also, the track is currently defined inside the track view for testing purposes.
    
    It's already possible to pan the view by holding the right mouse button and the display is also double-buffered.

---
+++ trunk/src/JACAVI/src/de/jacavi/rcp/widgets/TrackWidget.java
@@ -0,0 +1,150 @@
+package de.jacavi.rcp.widgets;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.DisposeEvent;
+import org.eclipse.swt.events.DisposeListener;
+import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.events.MouseListener;
+import org.eclipse.swt.events.MouseMoveListener;
+import org.eclipse.swt.events.PaintEvent;
+import org.eclipse.swt.events.PaintListener;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.GC;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.widgets.Canvas;
+import org.eclipse.swt.widgets.Composite;
+
+import de.jacavi.appl.valueobjects.Track;
+import de.jacavi.appl.valueobjects.TrackSection;
+import de.jacavi.rcp.Activator;
+
+
+
+public class TrackWidget extends Canvas {
+
+    private Image image;
+
+    private Point panPosition = new Point(200, 200);
+
+    private boolean isPanningActive = false;
+
+    private Point panningStartPosition = null;
+
+    private Track currentTrack = null;
+
+    public TrackWidget(Composite parent) {
+        super(parent, SWT.DOUBLE_BUFFERED);
+
+        final Color white = new Color(null, 255, 255, 255);
+        setBackground(white);
+
+        image = Activator.getImageDescriptor("/icons/strecke.jpg").createImage();
+
+        // HACK: hardcoded track for testing
+        int index = 1;
+        currentTrack = new Track();
+        currentTrack.insertSection(new TrackSection("straight.png", new Point(53, 0), 0), index++);
+        currentTrack.insertSection(new TrackSection("turn_90deg.png", new Point(0, -53), -90), index++);
+        currentTrack.insertSection(new TrackSection("straight.png", new Point(0, -25), 0), index++);
+        currentTrack.insertSection(new TrackSection("turn_90deg.png", new Point(-25 - 25, 0), -90), index++);
+        currentTrack.insertSection(new TrackSection("straight.png", new Point(-53, 0), 0), index++);
+        currentTrack.insertSection(new TrackSection("straight.png", new Point(-53, 0), 0), index++);
+        currentTrack.insertSection(new TrackSection("straight.png", new Point(-53, 0), 0), index++);
+        currentTrack.insertSection(new TrackSection("straight.png", new Point(-25, 0), 0), index++);
+        currentTrack.insertSection(new TrackSection("turn_90deg.png", new Point(0, 25), -90), index++);
+        currentTrack.insertSection(new TrackSection("straight.png", new Point(0, 53), 0), index++);
+        currentTrack.insertSection(new TrackSection("turn_90deg.png", new Point(25, 0), -90), index++);
+        currentTrack.insertSection(new TrackSection("straight.png", new Point(53, 0), 0), index++);
+
+        addPaintListener(new PaintListener() {
+            public void paintControl(PaintEvent e) {
+                TrackWidget.this.paintControl(e);
+            }
+        });
+
+        addMouseListener(new MouseListener() {
+            @Override
+            public void mouseDoubleClick(MouseEvent e) {
+                TrackWidget.this.handleMouseDoubleClick(e);
+            }
+
+            @Override
+            public void mouseDown(MouseEvent e) {
+                TrackWidget.this.handleMouseDown(e);
+            }
+
+            @Override
+            public void mouseUp(MouseEvent e) {
+                TrackWidget.this.handleMouseUp(e);
+            }
+        });
+
+        addMouseMoveListener(new MouseMoveListener() {
+            @Override
+            public void mouseMove(MouseEvent e) {
+                TrackWidget.this.handleMouseMove(e);
+            }
+        });
+
+        addDisposeListener(new DisposeListener() {
+            public void widgetDisposed(DisposeEvent e) {
+                white.dispose();
+            }
+        });
+    }
+
+    protected void handleMouseUp(MouseEvent e) {
+        if(e.button == 3) {
+            isPanningActive = false;
+            panningStartPosition = null;
+        }
+    }
+
+    protected void handleMouseDown(MouseEvent e) {
+        System.out.println("mousedown");
+        if(e.button == 3 && !isPanningActive) {
+            isPanningActive = true;
+            panningStartPosition = new Point(e.x, e.y);
+        }
+    }
+
+    protected void handleMouseMove(MouseEvent e) {
+        System.out.println("mousemove");
+        if(isPanningActive) {
+            System.out.println("pan");
+            panPosition.x += e.x - panningStartPosition.x;
+            panPosition.y += e.y - panningStartPosition.y;
+            panningStartPosition = new Point(e.x, e.y);
+            redraw();
+        }
+    }
+
+    protected void handleMouseDoubleClick(MouseEvent e) {
+    // x += 5;
+    // redraw();
+    }
+
+    protected void paintControl(PaintEvent e) {
+        GC gc = e.gc;
+
+        Point nextDrawingPosition = new Point(panPosition.x, panPosition.y);
+        int currentAngle = 0;
+        for(TrackSection s: currentTrack.getSections()) {
+            gc.drawImage(s.getRotatedImage(currentAngle), nextDrawingPosition.x, nextDrawingPosition.y);
+            currentAngle += s.getEntryToExitAngle();
+            if(currentAngle < 0)
+                currentAngle += 360;
+            if(currentAngle >= 360)
+                currentAngle -= 360;
+            nextDrawingPosition.x += s.getNextSectionOffset().x;
+            nextDrawingPosition.y += s.getNextSectionOffset().y;
+        }
+
+        /*
+         * if(image != null) { gc.drawImage(image, panPosition.x, panPosition.y); }
+         */
+
+        // gc.drawString("text", x, 1);
+    }
+}
