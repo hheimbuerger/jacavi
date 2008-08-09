618c17cef3297efd61dbea99993075a4c5bfe929
hheimbuerger <hheimbuerger@4031bf02-2d4d-0410-8032-e70447eaef8d>
2008-08-09 16:23:39 +0000

    [Related to ticket #5] Added a setter for the track name to the Track class and improved documentation.
    A setter has been added for the track name of the Track class (so it can be changed while running in designer mode) and the used tile set is now stored and can be retrieved.
    
    The javadoc documentation has been improved.
    
    NOTE: The class is no longer Serializable, as the loading and saving is done manually via member methods anyway.

--- trunk/src/JACAVI/src/de/jacavi/track/Track.java
+++ trunk/src/JACAVI/src/de/jacavi/track/Track.java
@@ -2,9 +2,9 @@ package de.jacavi.track;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.Serializable;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -22,32 +22,58 @@ import de.jacavi.track.TilesetRepository.TileSet;
 
 
 
-public class Track implements Serializable{
+/**
+ * Represents a slot car track.
+ * <p>
+ * The track has a specific name, a certain tile set to be used and a list of sections (parametrised tiles).
+ * <p>
+ * While the name and the list of sections can be modified, the tile set is static.
+ */
+public class Track {
 
     /**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-
-	public class TrackLoadingException extends Exception {
+     * The exception that is thrown when loading a track fails. A more detailed explanation of the cause can be found in
+     * the root exception.
+     */
+    public class TrackLoadingException extends Exception {
         public TrackLoadingException(Exception e) {
             super(e);
         }
     }
 
+    /** The tile set used for this track. */
+    private TileSet tileset;
+
+    /** The list of sections (tiles) this track consists of. */
     private List<TrackSection> sections = new LinkedList<TrackSection>();
 
+    /** The visible name of the track. */
     private String trackName;
-    
-    public Track() {
-		// TODO Auto-generated constructor stub
-	}
 
+    /**
+     * Constructor
+     * <p>
+     * Creates a new, empty Track using a given TileSet.
+     * 
+     * @param tileset
+     *            the tileset to be used for the track
+     */
     public Track(TileSet tileset) {
+        this.tileset = tileset;
         TilesetRepository tilesetRepository = (TilesetRepository) ContextLoader.getBean("tilesetRepository");
         sections.add(new TrackSection(tilesetRepository.getTile(tileset, "finishingStraight")));
     }
 
+    /**
+     * Constructor
+     * <p>
+     * Loads an existing track from a file, given as an input stream.
+     * 
+     * @param inputStream
+     *            the input stream to read the file from
+     * @throws TrackLoadingException
+     *             raised when loading the track fails
+     */
     public Track(InputStream inputStream) throws TrackLoadingException {
         try {
             loadFromXml(inputStream);
@@ -56,18 +82,46 @@ public class Track implements Serializable{
         }
     }
 
-    public Track(File file) throws TrackLoadingException {
-        try {
-            loadFromXml(new FileInputStream(file));
-        } catch(Exception e) {
-            throw new TrackLoadingException(e);
-        }
+    /**
+     * Constructor
+     * <p>
+     * Loads an existing track from a file, given as a file object.
+     * 
+     * @param file
+     *            the file to load
+     * @throws TrackLoadingException
+     *             raised when loading the track fails
+     * @throws FileNotFoundException
+     *             raised if the file can't be found
+     */
+    public Track(File file) throws TrackLoadingException, FileNotFoundException {
+        this(new FileInputStream(file));
     }
 
-    public void insertSection(Tile tile, int position) {
+    public TileSet getTileset() {
+        return tileset;
+    }
+
+    /**
+     * Inserts a new section (tile) at the given position.
+     * 
+     * @param tile
+     *            the tile to insert
+     * @param position
+     *            index at which the specified element is to be inserted
+     * @throws IndexOutOfBoundsException
+     *             raised if the position is invalid
+     */
+    public void insertSection(Tile tile, int position) throws IndexOutOfBoundsException {
         sections.add(position, new TrackSection(tile));
     }
 
+    /**
+     * Inserts a new section (tile) at the end of the track (after the last section).
+     * 
+     * @param tile
+     *            the tile to insert
+     */
     public void appendSection(Tile tile) {
         sections.add(new TrackSection(tile));
     }
@@ -80,8 +134,23 @@ public class Track implements Serializable{
         return sections;
     }
 
+    /**
+     * Returns the visible name of the track.
+     * 
+     * @return the name of the track
+     */
     public String getTrackName() {
         return trackName;
+    }
+
+    /**
+     * Sets the name of the track.
+     * 
+     * @param trackName
+     *            the new name of the track
+     */
+    public void setTrackName(String trackName) {
+        this.trackName = trackName;
     }
 
     public void saveToXml(String filename) {
@@ -102,7 +171,7 @@ public class Track implements Serializable{
         Document document = parseTrackFile(inputStream);
         trackName = document.getDocumentElement().getAttribute("tileset");
         String tilesetID = document.getDocumentElement().getAttribute("tileset");
-        TileSet tileset = TileSet.valueOf(tilesetID.toUpperCase());
+        tileset = TileSet.valueOf(tilesetID.toUpperCase());
 
         // read the sections
         sections.clear();
