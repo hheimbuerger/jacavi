7b9ad2a21389a59ee5a71f0b6c613cd542dd452b
frohn <frohn@4031bf02-2d4d-0410-8032-e70447eaef8d>
2008-08-09 14:43:08 +0000

    [Partial fix for ticket #32] Better structure of actions in Menu Bar

--- trunk/src/JACAVI/src/de/jacavi/rcp/ApplicationActionBarAdvisor.java
+++ trunk/src/JACAVI/src/de/jacavi/rcp/ApplicationActionBarAdvisor.java
@@ -1,18 +1,20 @@
 package de.jacavi.rcp;
 
+import org.eclipse.jface.action.GroupMarker;
 import org.eclipse.jface.action.IMenuManager;
 import org.eclipse.jface.action.MenuManager;
-import org.eclipse.ui.IWorkbenchActionConstants;
+import org.eclipse.jface.action.Separator;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.actions.ActionFactory;
 import org.eclipse.ui.actions.ActionFactory.IWorkbenchAction;
 import org.eclipse.ui.application.ActionBarAdvisor;
 import org.eclipse.ui.application.IActionBarConfigurer;
 
+
 public class ApplicationActionBarAdvisor extends ActionBarAdvisor {
 
 	private IWorkbenchAction exitAction;
-	private IWorkbenchAction loadTrackAction;
+	private IWorkbenchAction saveTrackAction;
 	private IWorkbenchAction newTrackAction;
 	private IWorkbenchAction aboutAction;
 
@@ -25,10 +27,10 @@ public class ApplicationActionBarAdvisor extends ActionBarAdvisor {
 		newTrackAction.setText("New");
 		register(newTrackAction);
 
-		loadTrackAction = ActionFactory.SAVE.create(window);
-		loadTrackAction.setText("Load");
-		register(loadTrackAction);
-		
+		saveTrackAction = ActionFactory.SAVE.create(window);
+		saveTrackAction.setText("Save");
+		register(saveTrackAction);
+
 		aboutAction = ActionFactory.ABOUT.create(window);
 		register(aboutAction);
 
@@ -37,28 +39,31 @@ public class ApplicationActionBarAdvisor extends ActionBarAdvisor {
 	}
 
 	protected void fillMenuBar(IMenuManager menuBar) {
-		MenuManager fileMenu = new MenuManager("&File",
-				IWorkbenchActionConstants.M_FILE);
+		MenuManager fileMenu = new MenuManager("&File");
 		menuBar.add(fileMenu);
-		fileMenu.add(newTrackAction);
 		fileMenu.add(exitAction);
+		
 
-		MenuManager trackMenu = new MenuManager("&Track");
+		MenuManager trackMenu = new MenuManager("&Track","trackMenu");
 		menuBar.add(trackMenu);
 		trackMenu.add(newTrackAction);
-		trackMenu.add(loadTrackAction);
+		trackMenu.add(new GroupMarker("trackGroup"));
+		trackMenu.add(new Separator());
+		trackMenu.add(saveTrackAction);
+
+		menuBar.add(new GroupMarker("other"));
 
 		MenuManager infoMenu = new MenuManager("&Help");
 		menuBar.add(infoMenu);
 		infoMenu.add(aboutAction);
 	}
-	
+
 //	protected void fillCoolBar(ICoolBarManager coolBar) {
 //		// This will add a new toolbar to the application
 //		IToolBarManager toolbar = new ToolBarManager(SWT.FLAT | SWT.RIGHT);
 //		coolBar.add(new ToolBarContributionItem(toolbar, "main"));
 //		// Add the entry to the toolbar
-////		toolbar.add(Factory.iSaveAction);
+//		 toolbar.add(Factory.iSaveAction);
 //	}
 
 }
