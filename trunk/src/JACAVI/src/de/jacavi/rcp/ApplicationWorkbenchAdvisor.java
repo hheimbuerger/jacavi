74afd7ce8081a4a84e8ac64ca06fa9daba09fac9
frohn <frohn@4031bf02-2d4d-0410-8032-e70447eaef8d>
2008-08-12 15:48:00 +0000

    [Partial fix for ticket #6] Track Designer after RCP start visible (only for debugging)

--- trunk/src/JACAVI/src/de/jacavi/rcp/ApplicationWorkbenchAdvisor.java
+++ trunk/src/JACAVI/src/de/jacavi/rcp/ApplicationWorkbenchAdvisor.java
@@ -1,23 +1,62 @@
 package de.jacavi.rcp;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
 import org.eclipse.ui.application.WorkbenchAdvisor;
 import org.eclipse.ui.application.WorkbenchWindowAdvisor;
 
-
+import de.jacavi.appl.track.Track;
+import de.jacavi.appl.track.Track.TrackLoadingException;
+import de.jacavi.rcp.editors.TrackDesigner;
+import de.jacavi.rcp.editors.TrackDesignerInput;
 
 public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
 
-    private static final String PERSPECTIVE_ID = "JACAVI.perspective";
+	private static final String PERSPECTIVE_ID = "JACAVI.perspective";
 
-    @Override
-    public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(IWorkbenchWindowConfigurer configurer) {
-        return new ApplicationWorkbenchWindowAdvisor(configurer);
-    }
+	@Override
+	public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(
+			IWorkbenchWindowConfigurer configurer) {
+		return new ApplicationWorkbenchWindowAdvisor(configurer);
+	}
 
-    @Override
-    public String getInitialWindowPerspectiveId() {
-        return PERSPECTIVE_ID;
-    }
+	@Override
+	public String getInitialWindowPerspectiveId() {
+		return PERSPECTIVE_ID;
+	}
 
+	// DEBUG!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	@Override
+	public void postStartup() {
+		super.postStartup();
+		TrackDesignerInput editorInput;
+		try {
+			editorInput = new TrackDesignerInput(new Track(new File(
+					"tracks/demo_crossing.track.xml")));
+
+			IWorkbenchPage page = PlatformUI.getWorkbench()
+					.getActiveWorkbenchWindow().getActivePage();
+
+			page.openEditor(editorInput, TrackDesigner.ID);
+
+			IWorkbenchPart active = page.getActivePart();
+			active.setFocus();
+
+		} catch (PartInitException e) {
+			// TODO: handle exception
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (TrackLoadingException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
+	}
 }
