052902b2d5d63071c9ee24001a6488abbfa7eadf
froth <froth@4031bf02-2d4d-0410-8032-e70447eaef8d>
2008-08-13 13:01:01 +0000

    [Related to ticket #16] changed FirstCarreraNativeLibraryFactor to support Spring Beans added lib42TachnologyBean

--- trunk/src/JACAVI/src/de/jacavi/hal/FirstCarreraNativeLibraryFactory.java
+++ trunk/src/JACAVI/src/de/jacavi/hal/FirstCarreraNativeLibraryFactory.java
@@ -1,28 +1,29 @@
 package de.jacavi.hal;
 
-import de.jacavi.hal.lib42.NativeLib42Adapter;
+import de.jacavi.appl.ContextLoader;
+
 
 
 public class FirstCarreraNativeLibraryFactory implements CarreraNativeLibraryFactory {
 
-	@Override
-	public TechnologyController initialiseCarreraLib(CarreraLibraryType type) {		
-		
-		TechnologyController halController=null;
-		
-		switch(type)
-		{
-			case lib42:
-				halController=new NativeLib42Adapter();
-			
-			break;	
-			
-			case bluerider:
-				//TODO
-				break;
-		}
-		
-		return halController;
-	}
+    @Override
+    public TechnologyController initialiseCarreraLib(CarreraLibraryType type) {
+
+        TechnologyController halController = null;
+
+        switch(type) {
+            case lib42:
+
+                halController = (TechnologyController) ContextLoader.getBean("lib42technologyBean");
+
+                break;
+
+            case bluerider:
+                // TODO
+                break;
+        }
+
+        return halController;
+    }
 
 }
