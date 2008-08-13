19f53caa03b34db02bb392026b3909e9337c1518
froth <froth@4031bf02-2d4d-0410-8032-e70447eaef8d>
2008-08-13 13:08:15 +0000

    [Related to ticket #34] implemented a timer in RaceEngine
    added jacavi.properties for parameter outsourcing
    added configurationBeans.xml

--- src/JACAVI/src/de/jacavi/appl/racelogic/RaceEngine.java
+++ src/JACAVI/src/de/jacavi/appl/racelogic/RaceEngine.java
@@ -1,9 +1,62 @@
 package de.jacavi.appl.racelogic;
 
-public class RaceEngine {
+import java.util.Timer;
+import java.util.TimerTask;
 
-    public RaceEngine(Race race) {
-    // TODO Auto-generated constructor stub
+import de.jacavi.appl.controller.CarController;
+import de.jacavi.appl.controller.ControllerSignal;
+import de.jacavi.hal.TechnologyController;
+
+
+
+public class RaceEngine extends TimerTask {
+
+    private Race race = null;
+
+    private Timer raceTimer = null;
+
+    private int raceTimerInterval = 0;
+
+    public Timer getRaceTimer() {
+        return raceTimer;
     }
 
+    public void setRaceTimerInterval(int raceTimerInterval) {
+        this.raceTimerInterval = raceTimerInterval;
+    }
+
+    public RaceEngine(Race race) {
+        // preconditions
+        if(race == null)
+            throw new IllegalArgumentException("race may not be null");
+
+        // needs
+        this.race = race;
+        raceTimer = new Timer();
+        startTimer();
+    }
+
+    private void startTimer() {
+        raceTimer.schedule(this, 0, raceTimerInterval);
+    }
+
+    public void stopTimer() {
+        raceTimer.cancel();
+    }
+
+    @Override
+    public void run() {
+
+        for(Player player: race.getPlayers()) {
+            int carID = player.getId();
+            CarController carController = player.getController();
+            TechnologyController technologyController = player.getTechnologyController();
+            ControllerSignal signal = carController.poll();
+            // change track
+            if(signal.trigger)
+                technologyController.toggleSwitch(carID);
+            // set new speed
+            technologyController.setSpeed(carID, signal.speed);
+        }
+    }
 }
