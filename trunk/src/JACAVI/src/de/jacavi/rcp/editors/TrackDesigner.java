26f6c6a1c2fdc7a51c63ef7712423382c5e08da5
frohn <frohn@4031bf02-2d4d-0410-8032-e70447eaef8d>
2008-08-18 18:28:00 +0000

    [Partial fix for ticket #6 and #42] Track Designer redesigned
    The Trackdesigner now instantiates only the Track Widget. The Tiles to enlarge the track are outsourced in the Tile Explorer View (maybe the name has to be reconsider). This view is always updated if the Active Editor change.
    The Tiles aren't more embedded into a Coolbar, but now in several Buttons, which could now be scrolled.

--- trunk/src/JACAVI/src/de/jacavi/rcp/editors/TrackDesigner.java
+++ trunk/src/JACAVI/src/de/jacavi/rcp/editors/TrackDesigner.java
@@ -3,9 +3,6 @@ package de.jacavi.rcp.editors;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -16,29 +13,18 @@ import org.eclipse.jface.dialogs.ErrorDialog;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.KeyAdapter;
 import org.eclipse.swt.events.KeyEvent;
-import org.eclipse.swt.events.SelectionAdapter;
-import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.CoolBar;
-import org.eclipse.swt.widgets.CoolItem;
-import org.eclipse.swt.widgets.ToolBar;
-import org.eclipse.swt.widgets.ToolItem;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IEditorSite;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.part.EditorPart;
 
-import de.jacavi.appl.ContextLoader;
 import de.jacavi.appl.track.Tile;
-import de.jacavi.appl.track.TilesetRepository;
 import de.jacavi.appl.track.Track;
-import de.jacavi.appl.track.TilesetRepository.TileSet;
 import de.jacavi.appl.track.Track.InitialTileMayNotBeRemoved;
-import de.jacavi.rcp.Activator;
 import de.jacavi.rcp.dlg.SafeSaveDialog;
 import de.jacavi.rcp.widgets.TrackWidget;
 
@@ -50,31 +36,9 @@ public class TrackDesigner extends EditorPart {
 
     public static String ID = "JACAVI.TrackDesigner";
 
-    private static final double TILE_IMAGE_SCALE = 0.5;
-
     boolean isDirty = false;
 
-    private TilesetRepository tilesetRepository;
-
     private Track currentTrack;
-
-    private final List<Image> usedImages = new ArrayList<Image>();
-
-    public TrackDesigner() {
-        tilesetRepository = (TilesetRepository) ContextLoader.getBean("tilesetRepository");
-    }
-
-    /**
-     * Dispose handler that is invoked when the editor is closed.
-     * <p>
-     * Used to dispose the loaded images, because each of them is managing an operating system resource.
-     */
-    @Override
-    public void dispose() {
-        super.dispose();
-        for(Image image: usedImages)
-            image.dispose();
-    }
 
     @Override
     protected void setInput(IEditorInput input) {
@@ -139,53 +103,6 @@ public class TrackDesigner extends EditorPart {
     public void createPartControl(Composite parent) {
         parent.setLayout(new GridLayout());
 
-        final Map<String, Tile> tileMap = tilesetRepository.getAvailableTiles(currentTrack.getTileset());
-
-        CoolBar tilesCoolbar = new CoolBar(parent, SWT.NONE);
-        tilesCoolbar.setLocked(true);
-
-        CoolItem singleCoolItem = new CoolItem(tilesCoolbar, SWT.NONE);
-
-        Composite tilesComposite = new Composite(tilesCoolbar, SWT.None);
-        tilesComposite.setLayout(new GridLayout(tileMap.size(), false));
-        tilesComposite.setLayoutData(new GridData(GridData.FILL_VERTICAL));
-
-        for(String tileID: tileMap.keySet()) {
-            Tile tile = tileMap.get(tileID);
-            Image image = Activator.getImageDescriptor(tile.getFilename()).createImage();
-            usedImages.add(image);
-
-            // Scale images
-            int width = image.getBounds().width;
-            int height = image.getBounds().height;
-            Image scaledImage = new Image(null, image.getImageData().scaledTo((int) (width * TILE_IMAGE_SCALE),
-                    (int) (height * TILE_IMAGE_SCALE)));
-
-            // Toolbar is embedded in a CoolItem
-            ToolBar toolbar = new ToolBar(tilesComposite, SWT.WRAP);
-            toolbar.setLayoutData(new GridData(GridData.FILL_VERTICAL));
-
-            ToolItem tileToolItem = new ToolItem(toolbar, SWT.PUSH);
-            tileToolItem.setToolTipText(tile.getName());
-
-            // HACK: This hack exists because the DEBUG TileSet is already unscaled small and has not to be scaled
-            if(currentTrack.getTileset().equals(TileSet.DEBUG)) {
-                tileToolItem.setImage(image);
-            } else {
-                tileToolItem.setImage(scaledImage);
-            }
-
-            tileToolItem.addSelectionListener(new SelectionAdapter() {
-                @Override
-                public void widgetSelected(SelectionEvent e) {
-                    handleAppendage(tileMap, e);
-                }
-            });
-        }
-        tilesComposite.pack();
-        singleCoolItem.setSize(tilesComposite.getSize());
-        singleCoolItem.setControl(tilesComposite);
-
         final TrackWidget trackWidget;
         try {
             trackWidget = new TrackWidget(parent, currentTrack);
@@ -213,12 +130,13 @@ public class TrackDesigner extends EditorPart {
         log.error("Save failed", e);
     }
 
-    public void setTilesetRepository(TilesetRepository tilesetRepository) {
-        this.tilesetRepository = tilesetRepository;
-    }
-
-    public TilesetRepository getTilesetRepository() {
-        return tilesetRepository;
+    /**
+     * @param tile
+     */
+    public void handleAppendage(Tile tile) {
+        currentTrack.appendSection(tile);
+        log.debug(tile.getName() + " inserted to track");
+        fireTrackModified();
     }
 
     /**
@@ -241,26 +159,13 @@ public class TrackDesigner extends EditorPart {
         }
     }
 
-    /**
-     * Appends a tile on the last position
-     * 
-     * @param tileMap
-     * @param e
-     */
-    private void handleAppendage(final Map<String, Tile> tileMap, SelectionEvent e) {
-        ToolItem selected = (ToolItem) e.widget;
-        for(String tileID: tileMap.keySet()) {
-            Tile tile = tileMap.get(tileID);
-            if(selected.getToolTipText().equals(tile.getName())) {
-                currentTrack.appendSection(tile);
-                log.debug(tile.getName() + " inserted to track");
-                fireTrackModified();
-            }
-        }
-    }
-
-    private void fireTrackModified() {
+    public void fireTrackModified() {
         isDirty = true;
         firePropertyChange(PROP_DIRTY);
     }
+
+    public Track getCurrentTrack() {
+        return currentTrack;
+    }
+
 }
--- trunk/src/JACAVI/src/de/jacavi/rcp/perspectives/Perspective.java
+++ trunk/src/JACAVI/src/de/jacavi/rcp/perspectives/Perspective.java
@@ -1,23 +1,21 @@
 package de.jacavi.rcp.perspectives;
 
-
-
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
 
-import de.jacavi.rcp.views.LapView;
+import de.jacavi.rcp.views.TileExplorer;
+
+
 
 public class Perspective implements IPerspectiveFactory {
 
+    public void createInitialLayout(IPageLayout layout) {
+        String editorArea = layout.getEditorArea();
+        layout.setEditorAreaVisible(true);
+        // layout.setFixed(true);
+        // layout.addView(LapView.ID, IPageLayout.BOTTOM, 0.8f, editorArea);
+        layout.addView(TileExplorer.ID, IPageLayout.LEFT, 0.31f, editorArea);
 
-	public void createInitialLayout(IPageLayout layout) {
-		String editorArea = layout.getEditorArea();
-		layout.setEditorAreaVisible(true);
-//		layout.setFixed(true);
-		layout.addView(LapView.ID, IPageLayout.BOTTOM, 0.8f, editorArea);
-//		layout.addView(TrackView.ID,  IPageLayout.LEFT, 0.3f, editorArea);
-		
-		
-	}
+    }
 
 }
