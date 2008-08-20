2d7f44862265e57c697bd1d4ea80473de18331f7
hheimbuerger <hheimbuerger@4031bf02-2d4d-0410-8032-e70447eaef8d>
2008-08-20 09:00:15 +0000

    [Partial fix for ticket #39] Added the device images to the dialog and created the basic layout.

--- trunk/src/JACAVI/src/de/jacavi/rcp/dlg/InputDeviceSettingsDialog.java
+++ trunk/src/JACAVI/src/de/jacavi/rcp/dlg/InputDeviceSettingsDialog.java
@@ -2,21 +2,79 @@ package de.jacavi.rcp.dlg;
 
 import org.eclipse.jface.dialogs.IMessageProvider;
 import org.eclipse.jface.dialogs.TitleAreaDialog;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.CLabel;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.RowLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
+
+import de.jacavi.rcp.Activator;
 
 
 
 public class InputDeviceSettingsDialog extends TitleAreaDialog {
 
+    private final Image imageKeyboard;
+
+    private final Image imageMouse;
+
+    private final Image imageGameController;
+
+    private final Image imageWiimote;
+
     public InputDeviceSettingsDialog(Shell parentShell) {
         super(parentShell);
+
+        // prepare the images
+        imageKeyboard = Activator.getImageDescriptor("/icons/input_devices/keyboard.png").createImage();
+        imageMouse = Activator.getImageDescriptor("/icons/input_devices/mouse.png").createImage();
+        imageGameController = Activator.getImageDescriptor("/icons/input_devices/game_controller.png").createImage();
+        imageWiimote = Activator.getImageDescriptor("/icons/input_devices/keyboard.png").createImage();
+    }
+
+    @Override
+    public boolean close() {
+        imageKeyboard.dispose();
+        imageMouse.dispose();
+        imageGameController.dispose();
+        imageWiimote.dispose();
+        return super.close();
     }
 
     @Override
     protected Control createDialogArea(Composite parent) {
-        return super.createDialogArea(parent);
+        Composite content = new Composite(parent, SWT.NONE);
+
+        // prepare the dialog layout
+        content.setLayoutData(new GridData(GridData.FILL_BOTH));
+
+        // prepare the composite layout
+        RowLayout layout = new RowLayout(SWT.HORIZONTAL);
+        layout.wrap = false;
+        layout.pack = true;
+        layout.justify = true;
+        layout.marginTop = 15;
+        layout.marginBottom = 200; // FIXME: just to make it look more like the final version
+        content.setLayout(layout);
+
+        // create the canvas and the vertical splitters
+        CLabel labelKeyboard = new CLabel(content, SWT.CENTER | SWT.SHADOW_NONE);
+        labelKeyboard.setImage(imageKeyboard);
+        new Label(content, SWT.SEPARATOR | SWT.VERTICAL | SWT.SHADOW_NONE | SWT.BORDER);
+        CLabel labelMouse = new CLabel(content, SWT.CENTER | SWT.SHADOW_NONE);
+        labelMouse.setImage(imageMouse);
+        new Label(content, SWT.SEPARATOR | SWT.VERTICAL | SWT.SHADOW_NONE | SWT.BORDER);
+        CLabel labelGameController = new CLabel(content, SWT.CENTER | SWT.SHADOW_NONE);
+        labelGameController.setImage(imageGameController);
+        new Label(content, SWT.SEPARATOR | SWT.VERTICAL | SWT.SHADOW_NONE | SWT.BORDER);
+        CLabel labelWiimote = new CLabel(content, SWT.CENTER | SWT.SHADOW_NONE);
+        labelWiimote.setImage(imageWiimote);
+
+        return content;
     }
 
     @Override
