2a1d7819716b4989b1f645b771263045332f23f8
froth <froth@4031bf02-2d4d-0410-8032-e70447eaef8d>
2008-08-21 08:36:47 +0000

    [Related to ticket #16] added some javadoc comments and descriptions

--- src/JACAVI/src/de/jacavi/hal/lib42/NativeCsdLib.java
+++ src/JACAVI/src/de/jacavi/hal/lib42/NativeCsdLib.java
@@ -1,20 +1,28 @@
 package de.jacavi.hal.lib42;
 
+/**
+ * @author Florian Roth
+ * <p>
+ *	loads the lib42 sensordetection lib and maps its functionality
+ */
 public class NativeCsdLib {
-	static
-	{
-		System.loadLibrary("CsdLib");
-	}
-	
-	/*initialization*/
-	public native int initSensorDetection();
-	public native void releaseSensorDetection();
-	
-	
-	//This is the fuction called by detecting a sensor
-	public static void callback(int carID,int sensorID)
-	{
-		//TODO: 
-	    System.out.println("callback: carID->" +carID + " sensorID->"+sensorID);
-	}
+    static {
+        System.loadLibrary("CsdLib");
+    }
+
+    /* initialization */
+    public native int initSensorDetection();
+
+    public native void releaseSensorDetection();
+
+    /**
+     * This function is called by the csdlib (Sensordetection Library) when a sensor is detected
+     * 
+     * @param carID
+     * @param sensorID
+     */
+    public static void callback(int carID, int sensorID) {
+        // TODO:
+        System.out.println("callback: carID->" + carID + " sensorID->" + sensorID);
+    }
 }
