1286f8d5b45337b2a639c47f13e69511abaf3262
froth <froth@4031bf02-2d4d-0410-8032-e70447eaef8d>
2008-08-21 08:36:47 +0000

    [Related to ticket #16] added some javadoc comments and descriptions

--- trunk/src/JACAVI/src/de/jacavi/hal/lib42/NativeLib42.java
+++ trunk/src/JACAVI/src/de/jacavi/hal/lib42/NativeLib42.java
@@ -1,50 +1,72 @@
 package de.jacavi.hal.lib42;
 
-
+/**
+ * @author fro 
+ * <p>
+ * Gives the interface on the native Clib42 library and loads it.
+ */
 public class NativeLib42 {
 
-	static
-	{
-		System.loadLibrary("Clib42");
-	}
-	
-	
-	public native int initLib42( int mode );
-	public native void releaseLib42();
-	/*
-	 * Methods to control a car and its track
-	 */
-	public native void setSpeed( int carID, int speed );
-	public native int getSpeed( int carID );
-	public native int toggleSwitch( int carID );
-	public native int getSwitch( int carID );
-	public native void fullBreak( int carID );
-	
-	//Handle Pace Car
-	public native void activatePacecar();
-	public native void deactivatePacecar();
-	public native void pacecar2box();
-	public native void setPacecarSwitch( int value );
-	public native int togglePacecarSwitch();
-	public native void setPcPitstop( int pitstop );
-	public native int isPacecarActive();
-	public native int getPcSwitch();
-	
-	// HANDLE PIT STOP
-	public native void setPitstop( int carId, int pitstop);
-	public native int togglePitstop( int carId ); 
-	public native int getPitstop( int carId );
+    static {
+        System.loadLibrary("Clib42");
+    }
 
-	// HANDLE FUEL FOR ALL CARS
-	public native void activateFuel();
-	public native void deactivateFuel();
+    public native int initLib42(int mode);
 
-	/* Methods to programm cars
-	 *TODO: what is all this
-	 */
-	public native void programmCar(int carID);
-	public native void programmSpeed( int value );
-	public native void programmBreak( int value );
-	public native void programmFuel( int valu );
-	public native void resetCars();
+    public native void releaseLib42();
+
+    /*
+     * Methods to control a car and its track
+     */
+    public native void setSpeed(int carID, int speed);
+
+    public native int getSpeed(int carID);
+
+    public native int toggleSwitch(int carID);
+
+    public native int getSwitch(int carID);
+
+    public native void fullBreak(int carID);
+
+    // Handle Pace Car
+    public native void activatePacecar();
+
+    public native void deactivatePacecar();
+
+    public native void pacecar2box();
+
+    public native void setPacecarSwitch(int value);
+
+    public native int togglePacecarSwitch();
+
+    public native void setPcPitstop(int pitstop);
+
+    public native int isPacecarActive();
+
+    public native int getPcSwitch();
+
+    // HANDLE PIT STOP
+    public native void setPitstop(int carId, int pitstop);
+
+    public native int togglePitstop(int carId);
+
+    public native int getPitstop(int carId);
+
+    // HANDLE FUEL FOR ALL CARS
+    public native void activateFuel();
+
+    public native void deactivateFuel();
+
+    /*
+     * Methods to programm carsTODO: what is all this
+     */
+    public native void programmCar(int carID);
+
+    public native void programmSpeed(int value);
+
+    public native void programmBreak(int value);
+
+    public native void programmFuel(int valu);
+
+    public native void resetCars();
 }
