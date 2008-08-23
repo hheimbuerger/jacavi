44a8fcfeeaaeaf615cfb605b3c849f3afbd5df88
hheimbuerger <hheimbuerger@4031bf02-2d4d-0410-8032-e70447eaef8d>
2008-08-23 22:48:10 +0000

    [Partial revert of change to ticket #51, related to ticket #39] Reverted accidental input device dialog size change from [187].

--- trunk/src/JACAVI/src/de/jacavi/rcp/dlg/InputDeviceSettingsDialog.java
+++ trunk/src/JACAVI/src/de/jacavi/rcp/dlg/InputDeviceSettingsDialog.java
@@ -91,7 +91,7 @@ public class InputDeviceSettingsDialog extends TitleAreaDialog {
 
     @Override
     protected Control createContents(Composite parent) {
-        getShell().setSize(700, 700);
+        getShell().setSize(700, 500);
         return super.createContents(parent);
     }
 
