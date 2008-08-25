4154504bc137785074ca869c353f6976c1ca4d1c
frohn <frohn@4031bf02-2d4d-0410-8032-e70447eaef8d>
2008-08-25 14:18:02 +0000

    [Fix for ticket #57] Wizard splitted into different Wizards
    I added an abstract class of NewTrackWizard, from which the other Wizards extends --> Less Code and common interface

---
+++ trunk/src/JACAVI/src/de/jacavi/rcp/wizards/NewDigitalTrackWizard.java
@@ -0,0 +1,12 @@
+package de.jacavi.rcp.wizards;
+
+import de.jacavi.appl.track.TilesetRepository.TileSet;
+
+
+
+public class NewDigitalTrackWizard extends NewTrackWizard {
+
+    public NewDigitalTrackWizard() {
+        super(TileSet.DIGITAL);
+    }
+}
--- trunk/src/JACAVI/src/de/jacavi/rcp/wizards/pages/CreateTrackPageOne.java
+++ trunk/src/JACAVI/src/de/jacavi/rcp/wizards/pages/CreateTrackPageOne.java
@@ -4,25 +4,19 @@ import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.KeyEvent;
 import org.eclipse.swt.events.KeyListener;
-import org.eclipse.swt.events.SelectionAdapter;
-import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
 
 import de.jacavi.appl.track.Track;
-import de.jacavi.appl.track.TilesetRepository.TileSet;
 
 
 
 public class CreateTrackPageOne extends WizardPage {
     private Text trackNameInputField;
-
-    private Combo comboTileSet;
 
     private Composite container;
 
@@ -55,34 +49,13 @@ public class CreateTrackPageOne extends WizardPage {
         GridData gd = new GridData(GridData.FILL_HORIZONTAL);
         trackNameInputField.setLayoutData(gd);
 
-        Label label2 = new Label(container, SWT.NONE);
-        label2.setText("Kind of slot car track: ");
-
-        comboTileSet = new Combo(container, SWT.BORDER | SWT.READ_ONLY);
-        String[] tileSets = new String[TileSet.values().length];
-        for(int i = 0; i < TileSet.values().length; i++) {
-            tileSets[i] = TileSet.values()[i].name();
-        }
-        comboTileSet.setItems(tileSets);
-        comboTileSet.addSelectionListener(new SelectionAdapter() {
-            @Override
-            public void widgetSelected(SelectionEvent e) {
-                checkComplete();
-            }
-        });
-
         // Required to avoid an error in the system
         setControl(container);
         setPageComplete(false);
-
     }
 
     public String getText1() {
         return trackNameInputField.getText();
-    }
-
-    public TileSet getTileSet() {
-        return TileSet.valueOf(comboTileSet.getText());
     }
 
     // You need to overwrite this method otherwise you receive a
@@ -95,11 +68,11 @@ public class CreateTrackPageOne extends WizardPage {
 
     private void checkComplete() {
         String input = trackNameInputField.getText();
-        if(input.endsWith(Track.FILE_EXTENSION) && input.length() > Track.FILE_EXTENSION.length()
-                && !comboTileSet.getText().isEmpty()) {
+        if(input.endsWith(Track.FILE_EXTENSION) && input.length() > Track.FILE_EXTENSION.length()) {
             setPageComplete(true);
         } else {
             setPageComplete(false);
         }
     }
+
 }
