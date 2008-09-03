b10e52efcd1c23dd5ac04cbb28c990f111567a74
frohn <frohn@4031bf02-2d4d-0410-8032-e70447eaef8d>
2008-09-03 18:34:24 +0000

    [Partial Fix for ticket #77] Added some 16x16 Images for each connector

--- trunk/src/JACAVI/src/de/jacavi/rcp/dlg/provider/PlayerLabelProvider.java
+++ trunk/src/JACAVI/src/de/jacavi/rcp/dlg/provider/PlayerLabelProvider.java
@@ -1,10 +1,17 @@
 package de.jacavi.rcp.dlg.provider;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.eclipse.jface.viewers.ILabelProviderListener;
 import org.eclipse.jface.viewers.ITableLabelProvider;
 import org.eclipse.swt.graphics.Image;
 
 import de.jacavi.appl.racelogic.Player;
+import de.jacavi.hal.analogue.AnalogueDriveConnector;
+import de.jacavi.hal.bluerider.BlueriderConnector;
+import de.jacavi.hal.lib42.Lib42DriveConnector;
+import de.jacavi.rcp.Activator;
 
 
 
@@ -12,8 +19,40 @@ public class PlayerLabelProvider implements ITableLabelProvider {
 
     public static String[] COLUMNNNAMES = { "Player Name", "Controller", "Connector" };
 
+    private final Map<String, Image> usedImages = new HashMap<String, Image>();
+
+    public PlayerLabelProvider() {
+        usedImages.put("analog", Activator.getImageDescriptor("/icons/connectors/analogue(16X16).png").createImage());
+        usedImages.put("bluerider", Activator.getImageDescriptor("/icons/connectors/bluerider(16X16).png")
+                .createImage());
+        usedImages.put("digital", Activator.getImageDescriptor("/icons/connectors/lib42(16X16).png").createImage());
+    }
+
     public Image getColumnImage(Object element, int columnIndex) {
-        // TODO here we could fill each column with a 16x16 px image (e.g. a small wiimote or a 42 logo)
+        Player player = (Player) element;
+
+        switch(columnIndex) {
+            case 0:
+
+                break;
+            case 1:
+
+                break;
+            case 2:
+                if(player.getSlotCarSystemConnector().getDriveConnector() instanceof AnalogueDriveConnector) {
+                    return usedImages.get("analog");
+                }
+                if(player.getSlotCarSystemConnector().getDriveConnector() instanceof Lib42DriveConnector) {
+                    return usedImages.get("digital");
+                }
+                if(player.getSlotCarSystemConnector().getDriveConnector() instanceof BlueriderConnector) {
+                    return usedImages.get("bluerider");
+                }
+                break;
+
+            default:
+                break;
+        }
         return null;
     }
 
