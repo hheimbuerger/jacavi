54191fb98f8b9741892850d840db3bf781bd743b
frohn <frohn@4031bf02-2d4d-0410-8032-e70447eaef8d>
2008-09-05 12:49:41 +0000

    [Partial fix for ticket #12] Added an initial MouseDevice Conroller
    The MouseDevice works with a global listener like the KeyBoaed Device(see #13). 
    For a clean solution the listener has to be added when the race starts and removed, when the race end up.(cleanup method)
    
    Maybe we really have to integrate an additionally MouseView, where the mouse has the focus on. Actually the current version works fine, but the MouseEvents of the TrackWidget compete against.

--- trunk/src/JACAVI/src/de/jacavi/appl/controller/device/impl/MouseDevice.java
+++ trunk/src/JACAVI/src/de/jacavi/appl/controller/device/impl/MouseDevice.java
@@ -1,13 +1,29 @@
 package de.jacavi.appl.controller.device.impl;
 
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Listener;
+
 import de.jacavi.appl.controller.ControllerSignal;
 import de.jacavi.appl.controller.device.DeviceController;
+import de.jacavi.rcp.widgets.TrackWidget;
 
 
 
-public class MouseDevice extends DeviceController {
+public class MouseDevice extends DeviceController implements Listener {
+
+    private final ControllerSignal currentControllerSignal;
+
     public MouseDevice(String name) {
         super(name);
+
+        currentControllerSignal = new ControllerSignal();
+
+        // TODO: Listener has to be added in initialize method
+        Display.getCurrent().addFilter(SWT.MouseDown, this);
+        Display.getCurrent().addFilter(SWT.MouseMove, this);
+        Display.getCurrent().addFilter(SWT.MouseUp, this);
     }
 
     @Override
@@ -18,19 +34,82 @@ public class MouseDevice extends DeviceController {
 
     @Override
     public ControllerSignal poll() {
-        return new ControllerSignal(50, false);
+        return currentControllerSignal;
     }
 
     @Override
     public void cleanup() {
-    // TODO Auto-generated method stub
-
+        Display.getCurrent().removeFilter(SWT.MouseDown, this);
     }
 
     @Override
     public int normaliseSpeedSignal(float deviceSpecificInputSpeedSignal) {
-        // TODO Auto-generated method stub
         return 0;
     }
 
+    private boolean pressed1 = false;
+
+    private int lastY = 0;
+
+    private int lastSpeed = 0;
+
+    @Override
+    public void handleEvent(Event event) {
+        // TODO HACK:under some untraceable circumstances, a focus on a combo in the PlayerSettingsDialog fires an
+        // event and results a
+        // strange behavior --> EventListener(or Filter) has to be added when race starts
+
+        if(event.widget instanceof TrackWidget) {
+
+            switch(event.button) {
+                // left mouse
+                case 1:
+                    if(!pressed1) {
+                        pressed1 = true;
+                        lastY = event.y;
+                        System.out.println(lastY);
+                        System.out.println(event.widget);
+                    } else if(pressed1) {
+                        pressed1 = false;
+                        lastSpeed = currentControllerSignal.getSpeed();
+                    }
+
+                    break;
+
+                // middle mouse
+                case 2:
+
+                    break;
+
+                // right mouse
+                case 3:
+                    handleTrigger();
+                    break;
+
+                default:
+                    if(pressed1) {
+                        if(lastY > event.y)
+                            handleSpeed(lastSpeed + Math.abs(event.y - lastY) - 1);
+                        else {
+                            handleSpeed(lastSpeed - Math.abs(event.y - lastY) - 1);
+                        }
+                    }
+                    break;
+            }
+        }
+    }
+
+    private void handleTrigger() {
+        boolean trigger = currentControllerSignal.isTrigger();
+        currentControllerSignal.setTrigger(!trigger);
+    }
+
+    private void handleSpeed(int speed) {
+        if(speed > 100)
+            speed = 100;
+        if(speed < 0)
+            speed = 0;
+        System.out.println("speed:" + speed);
+        currentControllerSignal.setSpeed(speed);
+    }
 }
