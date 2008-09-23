c673f35e0ed10949b8c8a0cb8a34364e3c165779
Henrik Heimbuerger <henrik@heimbuerger.de>
2008-09-23 12:22:19 +0000

    [Partial fix for ticket #22] Created dialog for reviewing recognised driving agent scripts.

---
+++ src/JACAVI/agents/SampleAgent.groovy
@@ -0,0 +1,7 @@
+import Main.ScriptInterface
+
+public class DrivingAgent implements ScriptInterface {
+    public String getOutput() {
+        return "Hello, Groovy world!"
+    }
+}
---
+++ src/JACAVI/agents/SampleAgent.py
@@ -0,0 +1,3 @@
+class DrivingAgent(ScriptInterface):
+    def getOutput(self):
+        return "Hello, Python world!"
