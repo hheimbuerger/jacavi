fdde76184e565a3ccbf66337d3e1c598021e4f39
froth <froth@4031bf02-2d4d-0410-8032-e70447eaef8d>
2008-09-24 11:40:28 +0000

    [Related to ticket #44] added the LGPL comment to the Bluerider ComManager
    added some comments to all the Bluerider classes

--- src/JACAVI/src/de/jacavi/hal/bluerider/ComException.java
+++ src/JACAVI/src/de/jacavi/hal/bluerider/ComException.java
@@ -1,24 +1,25 @@
+/* froth modified this in the way that there is no more GUI in here.
+ * On problems and qustions or for the original code apply to daniel@d-urbanietz.de or monagel@gmx.net 
+ */
+
 package de.jacavi.hal.bluerider;
 
-public class ComException extends Exception 
-{
-	/**
+public class ComException extends Exception {
+    /**
 	 * 
 	 */
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	public static int FAILSAFE = 0;
-	
-	private int reason;
-	
-	public ComException(int reason) 
-	{
-		this.reason = reason;
-		// TODO Auto-generated constructor stub
-	}
-	
-	public int getReason()
-	{
-		return reason;
-	}
+    public static int FAILSAFE = 0;
+
+    private int reason;
+
+    public ComException(int reason) {
+        this.reason = reason;
+        // TODO Auto-generated constructor stub
+    }
+
+    public int getReason() {
+        return reason;
+    }
 }
--- src/JACAVI/src/de/jacavi/hal/bluerider/Message.java
+++ src/JACAVI/src/de/jacavi/hal/bluerider/Message.java
@@ -1,12 +1,16 @@
+/* froth modified this in the way that there is no more GUI in here.
+ * On problems and qustions or for the original code apply to daniel@d-urbanietz.de or monagel@gmx.net 
+ */
+
 package de.jacavi.hal.bluerider;
 
-public class Message 
-{
-	public byte header;
-	public byte[] payload;
-	public Message() 
-	{
-		payload = new byte[2];
-		header = 0;
-	}
+public class Message {
+    public byte header;
+
+    public byte[] payload;
+
+    public Message() {
+        payload = new byte[2];
+        header = 0;
+    }
 }
