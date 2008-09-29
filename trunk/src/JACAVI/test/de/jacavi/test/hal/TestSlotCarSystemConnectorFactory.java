3b01656a431c8fdfef12419dd6a81a0e0af46a82
froth <froth@4031bf02-2d4d-0410-8032-e70447eaef8d>
2008-09-29 14:49:09 +0000

    [Related to ticket #113] added some validation to the start race validation
    now ever player needs a unique by uuid CarController
    and a unique by uuid SlotCarSystemConnector
    
    ignore changes of TestSlotCarSystemConnectorFactory and TestSimulationDriveConnectorAdapter

--- trunk/src/JACAVI/test/de/jacavi/test/hal/TestSlotCarSystemConnectorFactory.java
+++ trunk/src/JACAVI/test/de/jacavi/test/hal/TestSlotCarSystemConnectorFactory.java
@@ -4,13 +4,13 @@ import java.net.InetSocketAddress;
 
 import de.jacavi.hal.ConnectorFactory;
 import de.jacavi.hal.SlotCarSystemConnector;
-import de.jacavi.hal.simulation.SimulationDriveConnectorAdapter;
 import de.jacavi.hal.simulation.SimulationFeedbackConnectorAdapter;
 import de.jacavi.test.hal.connectors.TestAnalogueDriveConnectorAdapter;
 import de.jacavi.test.hal.connectors.TestAnalogueFeddbackConnectorAdapter;
 import de.jacavi.test.hal.connectors.TestBlueriderDriveConnectorAdapter;
 import de.jacavi.test.hal.connectors.TestBlueriderFeedbackConnectorAdapter;
 import de.jacavi.test.hal.connectors.TestLib42DriveConnectorAdapter;
+import de.jacavi.test.hal.connectors.TestSimulationDriveConnectorAdapter;
 import de.jacavi.test.hal.connectors.Testlib42FeedbackConnectorAdapter;
 
 
@@ -34,7 +34,7 @@ public class TestSlotCarSystemConnectorFactory implements ConnectorFactory {
     }
 
     public SlotCarSystemConnector createSimulatedConnector(String name) {
-        return new SlotCarSystemConnector(name, new SimulationDriveConnectorAdapter(name),
+        return new SlotCarSystemConnector(name, new TestSimulationDriveConnectorAdapter(name),
                 new SimulationFeedbackConnectorAdapter(name));
     }
 }
