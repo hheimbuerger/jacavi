77c142f85aa04ccd029e2f6196e7d9bbc0989c18
froth <froth@4031bf02-2d4d-0410-8032-e70447eaef8d>
2008-10-02 19:16:45 +0000

    [Related to ticket #17] added a Testlib42FeedbackConnectorAdapter to simulate feedback from lib42

--- trunk/src/JACAVI/test/de/jacavi/test/hal/connectors/Testlib42FeedbackConnectorAdapter.java
+++ trunk/src/JACAVI/test/de/jacavi/test/hal/connectors/Testlib42FeedbackConnectorAdapter.java
@@ -1,5 +1,10 @@
 package de.jacavi.test.hal.connectors;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+
 import de.jacavi.hal.FeedbackSignal;
 import de.jacavi.hal.lib42.Lib42FeedbackConnector;
 
@@ -7,27 +12,53 @@ import de.jacavi.hal.lib42.Lib42FeedbackConnector;
 
 public class Testlib42FeedbackConnectorAdapter implements Lib42FeedbackConnector {
 
-    private int carid;
+    private int carID;
+
+    private List<String> feedbackList = new ArrayList<String>();
+
+    private int feedbackIndex = 1;
+
+    private boolean start = false;
 
     public Testlib42FeedbackConnectorAdapter(int carID) {
-        this.carid = carID;
+        this.carID = carID;
+        initFeedback();
     }
 
-    @Override
-    public int getCarID() {
-        return carid;
-    }
-
-    @Override
-    public void sensorCallback(int sensorID) {
-    // TODO Auto-generated method stub
-
+    private void initFeedback() {
+        // on the htwg digital track we have sensorst from 1-16
+        for(int i = 1; i <= 15; i += 2) {
+            feedbackList.add(i + "");
+        }
     }
 
     @Override
     public FeedbackSignal pollFeedback() {
-        // TODO Auto-generated method stub
-        return null;
+        if(!start) {
+            start = true;
+            Timer timer = new Timer();
+            timer.schedule(new FeedbackTimerTask(), 10, 2000);
+        }
+        return new FeedbackSignal(null, feedbackIndex + "");
+    }
+
+    @Override
+    public int getCarID() {
+        return carID;
+    }
+
+    @Override
+    public void sensorCallback(int sensorID) {
+
+    }
+
+    class FeedbackTimerTask extends TimerTask {
+        @Override
+        public void run() {
+            feedbackIndex += 2;
+            if(feedbackIndex > 15)
+                feedbackIndex = 1;
+        }
     }
 
 }
