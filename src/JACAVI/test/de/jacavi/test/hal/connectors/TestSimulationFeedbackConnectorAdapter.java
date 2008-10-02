d83064f07ea739226fb6ec876490d9d893f21cb3
froth <froth@4031bf02-2d4d-0410-8032-e70447eaef8d>
2008-10-02 19:16:45 +0000

    [Related to ticket #17] added a Testlib42FeedbackConnectorAdapter to simulate feedback from lib42

--- src/JACAVI/test/de/jacavi/test/hal/connectors/TestSimulationFeedbackConnectorAdapter.java
+++ src/JACAVI/test/de/jacavi/test/hal/connectors/TestSimulationFeedbackConnectorAdapter.java
@@ -1,8 +1,5 @@
 package de.jacavi.test.hal.connectors;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import de.jacavi.hal.FeedbackSignal;
 import de.jacavi.hal.SlotCarFeedbackConnector;
 
@@ -12,28 +9,13 @@ public class TestSimulationFeedbackConnectorAdapter implements SlotCarFeedbackCo
 
     private String name;
 
-    private List<String> feedbackList = new ArrayList<String>();
-
-    private int feedbackIndex = 0;
-
     public TestSimulationFeedbackConnectorAdapter(String name) {
         this.name = name;
-        initFeedback();
-    }
-
-    private void initFeedback() {
-        for(int i = 0; i < 27; i++) {
-            feedbackList.add(i + "");
-        }
     }
 
     @Override
     public FeedbackSignal pollFeedback() {
-        int tmpIndex = feedbackIndex;
-        feedbackIndex++;
-        if(feedbackIndex >= 6)
-            feedbackIndex = 0;
-        return new FeedbackSignal(null, feedbackList.get(tmpIndex));
+        return new FeedbackSignal(null, "0");
     }
 
 }
