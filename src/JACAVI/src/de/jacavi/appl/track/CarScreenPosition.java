63f78c5f640171d07de0ff3a72e653badb50a7ed
Henrik Heimbuerger <henrik@heimbuerger.de>
2008-10-04 19:11:23 +0000

    [Fix for ticket #98] Cars are now drawn over all track tiles.
    This removes the drawing artifacts happening at the tile edges. However, it also removes the 'feature' of cars being drawn below tile 'bridges'. This might have to be investigated again, but for now, it's probably an improvement.
    
    Also removed old debug car and fixed a bug in the 'cloverleaf' tile of the debug tileset.

--- src/JACAVI/src/de/jacavi/appl/track/CarScreenPosition.java
+++ src/JACAVI/src/de/jacavi/appl/track/CarScreenPosition.java
@@ -3,12 +3,12 @@ package de.jacavi.appl.track;
 public class CarScreenPosition {
     public TrackSection section;
 
-    public DirectedPoint point;
+    public DirectedPoint directedPoint;
 
     public CarScreenPosition(TrackSection section, DirectedPoint point) {
         assert section != null;
         assert point != null;
         this.section = section;
-        this.point = point;
+        this.directedPoint = point;
     }
 }
