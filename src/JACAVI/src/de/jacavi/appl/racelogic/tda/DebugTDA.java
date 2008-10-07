caa01bc34a8d7b72791c5337f92081bb1e614d4d
froth <froth@4031bf02-2d4d-0410-8032-e70447eaef8d>
2008-10-07 19:11:56 +0000

    [Related to ticket #17] This was forgotten in the last changeset of #17

--- src/JACAVI/src/de/jacavi/appl/racelogic/tda/DebugTDA.java
+++ src/JACAVI/src/de/jacavi/appl/racelogic/tda/DebugTDA.java
@@ -18,6 +18,8 @@ public class DebugTDA extends TrackDataApproximator {
 
     private int lastGametick = 0;
 
+    private double acceleration = 0.0;
+
     public DebugTDA(Track track, int racetimerInterval) {
         this.track = track;
         this.raceTimerInterval = racetimerInterval;
@@ -30,42 +32,38 @@ public class DebugTDA extends TrackDataApproximator {
         if(feedbackSignal.getGforce() != null)
             logger.debug("Sensor: " + feedbackSignal.getLastCheckpoint() + " GForce: "
                     + feedbackSignal.getGforce().getX() + " " + feedbackSignal.getGforce().getY());
-        /*else
-            logger.debug("Sensor: " + feedbackSignal.getLastCheckpoint());*/
 
-        if(carPosition.isOnTrack) {
+        // car acceleration factor is defined in car.xml
+        double thrust = controllerSignal.getSpeed() * car.getAcceleration();
 
-            // adjusted speed: between -0.5 and +0.5
-            // double adjustedThrust = ((double) controllerSignal.getSpeed() / 100 - 0.5);
-            // acceleration = speed * car acceleration factor
-            double thrust = controllerSignal.getSpeed() / 100.0;
-            double thrustAcceleration = thrust * car.getAcceleration();
+        // calculate the friction 0.01 is car on concrete
+        double friction = (car.getMass() * 0.01) * -1;
 
-            double frictionAcceleration = car.getMass() * -1;
+        acceleration = thrust / car.getMass();
+        speed = Math.max(Math.min((acceleration * raceTimerInterval) + (friction * raceTimerInterval) + speed, car
+                .getTopSpeed()), 0);
 
-            // speed = old speed + acceleration * num adjustments (but limited to 0..topspeed)
-            speed = Math.max(Math.min(speed + thrustAcceleration * (gametick - lastGametick)
-                    + (frictionAcceleration * (gametick - lastGametick)), car.getTopSpeed()), 0);
-
-            System.out.println("speed=" + speed + ", thrust=" + thrustAcceleration + ", friction="
-                    + frictionAcceleration);
-
-            // assume the car leaves the track when driving at 80% top speed
-            if(speed > car.getTopSpeed() * 0.8)
-                carPosition.leaveTrack();
-            else {
-                // determine the new position;
-                int stepsToMove = ((int) speed);
-
-                /*logger.debug("acceleration (" + acceleration + "), speed (" + speed + "), stepsToMove (" + stepsToMove
-                        + "), topSpeed (" + car.getTopSpeed() + ")");*/
-
-                // move
-                carPosition.moveSteps(track, stepsToMove, controllerSignal.isTrigger());
-            }
-
+        if(speed > getMaxSpeed(controllerSignal.getSpeed(), car)) {
+            if(getMaxSpeed(controllerSignal.getSpeed(), car) == 0)
+                speed = speed--;
+            else
+                speed = getMaxSpeed(controllerSignal.getSpeed(), car);
         }
 
+        if(speed > car.getTopSpeed() * 0.9)
+            carPosition.leaveTrack();
+        else
+            // move
+            carPosition.moveSteps(track, (int) speed, controllerSignal.isTrigger());
+
         lastGametick = gametick;
+    }
+
+    private double getMaxSpeed(int controllerSignal, Car car) {
+        double result = 0;
+
+        result = (car.getTopSpeed() / 100) * controllerSignal;
+
+        return result;
     }
 }
