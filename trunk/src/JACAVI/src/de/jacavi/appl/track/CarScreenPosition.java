c2889039b32d69e1d183431f3b782e347c7230b7
hheimbuerger <hheimbuerger@4031bf02-2d4d-0410-8032-e70447eaef8d>
2008-10-07 08:39:02 +0000

    [Partial fix for ticket #102] Implemented simple visualisation for a car that has left the track.
    Additionally, the flag isOnTrack of the CarPosition is now actually used and probably needs to be checked in more situations in the future.

--- trunk/src/JACAVI/src/de/jacavi/appl/track/CarScreenPosition.java
+++ trunk/src/JACAVI/src/de/jacavi/appl/track/CarScreenPosition.java
@@ -1,14 +1,47 @@
 package de.jacavi.appl.track;
 
 public class CarScreenPosition {
-    public TrackSection section;
+    private final TrackSection section;
 
-    public DirectedPoint directedPoint;
+    private final DirectedPoint directedPoint;
 
+    private final boolean isOnTrack;
+
+    /**
+     * Constructor for a car that is off-track.
+     */
+    public CarScreenPosition() {
+        this.section = null;
+        this.directedPoint = null;
+        this.isOnTrack = false;
+    }
+
+    /**
+     * Constructor for a car that is on-track.
+     * 
+     * @param section
+     *            the section the car is at, may not be null
+     * @param point
+     *            the point
+     */
     public CarScreenPosition(TrackSection section, DirectedPoint point) {
         assert section != null;
         assert point != null;
         this.section = section;
         this.directedPoint = point;
+        this.isOnTrack = true;
     }
+
+    public boolean isOnTrack() {
+        return isOnTrack;
+    }
+
+    public TrackSection getSection() {
+        return section;
+    }
+
+    public DirectedPoint getDirectedPoint() {
+        return directedPoint;
+    }
+
 }
