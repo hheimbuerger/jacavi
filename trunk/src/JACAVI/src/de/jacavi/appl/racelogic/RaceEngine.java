50b1058e28b4eb858ba9acb6c3c7120d34eb7675
frohn <frohn@4031bf02-2d4d-0410-8032-e70447eaef8d>
2008-10-08 18:22:27 +0000

    [Fix for ticket #14] Fix that Traffic Lights has been shown two times

--- trunk/src/JACAVI/src/de/jacavi/appl/racelogic/RaceEngine.java
+++ trunk/src/JACAVI/src/de/jacavi/appl/racelogic/RaceEngine.java
@@ -16,7 +16,6 @@ import de.jacavi.appl.track.Statistics;
 import de.jacavi.appl.track.Track;
 import de.jacavi.hal.FeedbackSignal;
 import de.jacavi.hal.SlotCarSystemConnector;
-import de.jacavi.rcp.dlg.TrafficLightsDialog;
 import de.jacavi.rcp.util.Check;
 import de.jacavi.rcp.views.RaceView;
 
@@ -72,7 +71,6 @@ public class RaceEngine {
             // TODO: assign all cars to their starting position
             int i = 0;
             StartingPoint[] startingPoints = track.getStartingPoints();
-            new TrafficLightsDialog();
             for(Player player: players) {
                 player.getPosition().reset(startingPoints[i++]);
                 // inject each player with an specific tda and give activeTrack
